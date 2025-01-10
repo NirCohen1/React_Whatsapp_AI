acb52a99e450093c3ecb60b221ac4fb17d0f1392
NirCohen1 <nircohen133@gmail.com>
2025-01-10 18:47:35 +0200

    First commit

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import './App.css';
+
+import SingleFileUploader from './components/SingleFileUploader';
+
+function App() {
+  return (
+    <>
+      <h1>React File Upload</h1>
+
+      <SingleFileUploader />
+
+      <p className="read-the-docs">This app is built with Vite and React.</p>
+    </>
+  );
+}
+
+export default App;
